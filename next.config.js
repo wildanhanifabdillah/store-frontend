edbdaefe1337a5ab7411b1c0f525a025aff72ebb
wildanhanifabdillah <wildanhanifabdillah27@gmail.com>
2025-12-22 04:07:21 +0700

    ci: fix js config

---
+++ next.config.js
@@ -0,0 +1,49 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ggwp.id",
+      },
+      {
+        protocol: "https",
+        hostname: "www.ggwp.id",
+      },
+      {
+        protocol: "https",
+        hostname: "image.ggwp.id",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.ggwp.id",
+      },
+      {
+        protocol: "https",
+        hostname: "bluestacks.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.bluestacks.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.bluestacks.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn-www.bluestacks.com",
+      },
+    ],
+  },
+
+  experimental: {
+    turbo: false,
+  },
+}
+
+module.exports = nextConfig
